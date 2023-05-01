86f0cc8e9e9f4ea9c7a4f3c3d202cf1a7bbf458d
RevoltzDeveloper <joselucasdl993@gmail.com>
2023-05-01 16:42:09 -0300

    uptade

--- src/core/server/main.lua
+++ src/core/server/main.lua
@@ -1,49 +1,43 @@
 local db = dbConnect ("mysql", "host=127.0.0.1; port=3306", "root", "")
 
 addEventHandler("onResourceStart", getResourceRootElement(getThisResource()), function()
-    --dbExec (db, "DROP TABLE InfoAccounts")
     dbExec (db, "CREATE DATABASE IF NOT EXISTS `IslaRP` ")
     dbExec (db, "USE `IslaRP`")
-	dbExec (db, "CREATE TABLE IF NOT EXISTS InfoAccounts (ID INTEGER PRIMARY KEY, charName TEXT, charSubName TEXT, charAge INTEGER NOT NULL, accSerial TEXT)")
+	dbExec (db, "CREATE TABLE IF NOT EXISTS InfoAccounts (ID INTEGER PRIMARY KEY, charName TEXT, charSubName TEXT, charAge INTEGER NOT NULL, accSerial TEXT, charCity TEXT)")
 	print ("Table created.")
 end)
 
 addEventHandler('onPlayerJoin', getResourceRootElement(getThisResource()),
     function ()
         dbExec (db, "USE `IslaRP`")
-        triggerClientEvent('IslaRP:drawAccountsSvg', source)
     end
 )
 
-function NewPlayerCharacter (ID, charName, charSubName, charAge, accSerial, charCity )
-    local check = dbQuery (db, "SELECT * FROM InfoAccounts WHERE accSerial=?", accSerial)
-    response = dbPoll(check, -1)
-
-    if #response == 3 then
-        print("Usuário tem o número máximo de contas")
-        return "Erro"
-    else
-        dbExec (db, "INSERT INTO InfoAccounts (ID, charName, charSubName, charAge, accSerial) VALUES(?,?,?,?,?)", ID, charName, charSubName, charAge, accSerial)
-        return "Criado"
+addCommandHandler('createcharacter',
+    function()
+        local check = dbQuery (db, "SELECT * FROM InfoAccounts WHERE accSerial=?", accSerial)
+        response = dbPoll(check, -1)
+        if #response == 3 then
+            print("Usuário tem o número máximo de contas")
+            return "Erro"
+        else
+            dbExec (db, "INSERT INTO InfoAccounts (ID, charName, charSubName, charAge, accSerial) VALUES(?,?,?,?,?)", ID, charName, charSubName, charAge, accSerial)
+            return "Criado"
+        end
     end
-end
+)
 
-addEvent("Add1", true)
-addEventHandler("Add1", getRootElement(), NewPlayerCharacter)
-
-function CheckPlayerCharacterAmount (Serial)
-
-    local check = dbQuery (db, "SELECT * FROM InfoAccounts WHERE accSerial=?", Serial)
-    response = dbPoll(check, -1)
-
-    if #response ~= 0 then
-        print(#response)
-        return #reponse
-    else
-        print("O usuário não tem conta")
-        return 0
+addEvent("IslaRP:CheckPlayerHasCharacter", true)
+addEventHandler("IslaRP:CheckPlayerHasCharacter", getRootElement()
+    function()
+        local check = dbQuery (db, "SELECT * FROM InfoAccounts WHERE accSerial=?", Serial)
+        response = dbPoll(check, -1)
+        if #response ~= 0 then
+            print(#response)
+            return #reponse
+        else
+            print("O usuário não tem conta")
+            return 0
+        end
     end
-end
-
-addEvent("Check1", true)
-addEventHandler("Check1", getRootElement(), CheckPlayerCharacterAmount)
+)
